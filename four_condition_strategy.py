6bbf7f75afffdde90080376ebf81b7097482e77e
zombielu <ludaiche@gmail.com>
2025-09-26 00:52:47 -0400

    modify - capable of making decisions based on 4 condition strategy, but currently moving average 9,13,21 are read from original data

--- four_condition_strategy.py
+++ four_condition_strategy.py
@@ -3,58 +3,25 @@ from backtesting import Backtest, Strategy
 
 class FourConditionStrategy(Strategy):
     def init(self):
-        self.cond1 = False
-        self.cond2 = False
-        self.cond3 = False
+        self.cond_1_3 = False
         self.time_list = []
-
-        # 用于卖出条件
-        self.a1_high = None
-        self.a2_open = None
-        self.a2_close = None
-        self.a3_open = None
-        self.a3_close = None
-        self.a3_low = None
-        self.b1_open = None
-        self.b1_close = None
-        self.b2_close = None
+        self.curr_time = None
+        self.rectangle = []
         self.buy_a2_open = None  # 买入时对应的 a2.open
-
-    def clean(self):
-        self.cond1 = False
-        self.cond2 = False
-        self.cond3 = False
-        self.time_list = []
-        self.a1_high = None
-        self.a2_open = None
-        self.a2_close = None
-        self.a3_open = None
-        self.a3_close = None
-        self.a3_low = None
-        self.b1_open = None
-        self.b1_close = None
-        self.b2_close = None
 
 
     def is_valid_time(self, t):
-        blocks = [
-            (3, 6, 30),
-            (8, 11, 0),
-            (20, 22, 0)
-        ]
-        for start_h, end_h, end_m in blocks:
+        blocks = {1: (3, 6, 30), 2: (8, 11, 0), 3: (20, 22, 0)}
+        for idx, value in blocks.items():
+            start_h, end_h, end_m = value
             if (t.hour > start_h or (t.hour == start_h and t.minute >= 0)) and \
                 (t.hour < end_h or (t.hour == end_h and t.minute <= end_m)):
-                return True
-        return False
+                return idx
+        return None
 
 
     def in_same_block(self, times):
-        blocks = [
-            (3, 6, 30),
-            (8, 11, 0),
-            (20, 22, 0)
-        ]
+        blocks = [(3, 6, 30), (8, 11, 0), (20, 22, 0)]
         for start_h, end_h, end_m in blocks:
             if all((t.hour > start_h or (t.hour == start_h and t.minute >= 0)) and
                    (t.hour < end_h or (t.hour == end_h and t.minute <= end_m)) for t in times):
@@ -68,7 +35,6 @@ class FourConditionStrategy(Strategy):
             # 条件 1: close < a2.open
             if self.data.Close[-1] < self.buy_a2_open:
                 self.position.close()
-                print("sell with price: ", self.data.Close[-1])
                 self.buy_a2_open = None
                 return
             # 条件 2: close < MA3
@@ -76,7 +42,6 @@ class FourConditionStrategy(Strategy):
                 self.data.Close[-1] < self.data['Moving Average 3'][-1]
             ):
                 self.position.close()
-                print("sell with price: ", self.data.Close[-1])
                 self.buy_a2_open = None
                 return
 
@@ -87,75 +52,101 @@ class FourConditionStrategy(Strategy):
         # a1, a2, a3 = i - 2, i - 1, i
 
         # 条件 1: a1.high < a3.low
-        if self.data.High[-3] < self.data.Low[-1]:
-            if not self.is_valid_time(pd.to_datetime(self.data['time'][-1])):
-                return
-            self.cond1 = True
+        # 条件 3: MA1 > MA2 > MA3
+        if (self.data.High[-3] < self.data.Low[-1] and
+        all([self.data['Moving Average 1'][j] > self.data['Moving Average 2'][j] > self.data['Moving Average 3'][j]
+             for j in [-3, -2, -1]])):
+            self.curr_time = self.is_valid_time(pd.to_datetime(self.data['time'][-1]))
             self.time_list.extend([pd.to_datetime(self.data['time'][-1]),
                                    pd.to_datetime(self.data['time'][-2]),
                                    pd.to_datetime(self.data['time'][-3])])
-            self.a1_high = self.data.High[-3]
-            self.a2_open = self.data.Open[-2]
-            self.a2_close = self.data.Close[-2]
-            self.a3_open = self.data.Open[-1]
-            self.a3_close = self.data.Close[-1]
-            self.a3_low = self.data.Low[-1]
+            if not self.curr_time or not self.in_same_block(self.time_list):
+                self.curr_time = None
+                self.time_list = []
+                return
+
+            self.cond_1_3 = True
+            o1 = self.data.Open[-3]
+            c1 = self.data.Close[-3]
+            h1 = self.data.High[-3]
+            l1 = self.data.Low[-3]
+            o2 = self.data.Open[-2]
+            c2 = self.data.Close[-2]
+            h2 = self.data.High[-2]
+            l2 = self.data.Low[-2]
+            o3 = self.data.Open[-1]
+            c3 = self.data.Close[-1]
+            h3 = self.data.High[-1]
+            l3 = self.data.Low[-1]
+            # ------ only for checking -----------
+            ma19 = self.data['Moving Average 1'][-3]
+            ma113 = self.data['Moving Average 2'][-3]
+            ma121 = self.data['Moving Average 3'][-3]
+            ma29 = self.data['Moving Average 1'][-2]
+            ma213 = self.data['Moving Average 2'][-2]
+            ma221 = self.data['Moving Average 3'][-2]
+            ma39 = self.data['Moving Average 1'][-1]
+            ma313 = self.data['Moving Average 2'][-1]
+            ma321 = self.data['Moving Average 3'][-1]
+            self.rectangle.append((o1,c1,h1,l1,o2,c2,h2,l2,o3,c3,h3,l3,ma19,ma113,ma121,ma29,ma213,ma221,ma39,ma313,ma321,self.data['time'][-1]))
+            self.time_list = []
             # print("cond1 is True")
 
-        # 条件 3: MA1 > MA2 > MA3
-        if self.cond1 and all([self.data['Moving Average 1'][j] > self.data['Moving Average 2'][j] > self.data['Moving Average 3'][j] for j in [-3, -2, -1]]):
-            self.cond3 = True
-            # print("cond3 is True")
-        else:
-            self.clean()
+        if not self.cond_1_3:
+            return
 
-        # 条件 2: 找 b1
-        # case 2
-        if self.cond1 and self.data.Low[-1] < self.a3_low:
-            self.b1_close = self.data.Close[-1]
-            if self.b1_close > min(self.a3_open, self.a3_close):
-                self.cond2 = True
-                self.time_list.append(pd.to_datetime(self.data['time'][-1]))
+        # 条件 4:
+        if self.is_valid_time(
+                pd.to_datetime(self.data['time'][-1])) != self.curr_time:
+            self.rectangle = []
+            self.curr_time = None
+            self.cond_1_3 = False
+            return
+
+        for related in reversed(self.rectangle):
+            # o1, c1, h1, l1, o2, c2, h2, l2, o3, c3, h3, l3 = related
+            o1, c1, h1, l1, o2, c2, h2, l2, o3, c3, h3, l3, ma19,ma113,ma121,ma29,ma213,ma221,ma39,ma313,ma321,atime = related
+            # case 2
+            if self.data.Low[-1] < l3:
+                b1_close = self.data.Close[-1]
+                b1_open = self.data.Open[-1]
+                if b1_open > l3 and b1_close > min(o3, c3):
+                    self.buy_a2_open = o2
+                    self.buy()
+                    print("------case 2---- 4 condition met")
+                    print("b1_low:", self.data.Low[-1])
+                    print("b1_close:", b1_close)
+                    print("b1_open:", b1_open)
+                    print("b_time:", self.data['time'][-1])
+                    print(related)
+                    # raise IndexError
+                    return
                 # print("cond2 is True")
 
-        if self.cond1 and self.data.Low[-2] < self.a3_low:
-            self.b2_close = self.data.Close[-1]
-            self.b1_close = self.data.Close[-2]
-            self.b1_open = self.data.Open[-2]
-            print("min(self.b1_open, self.b1_close)", min(self.b1_open, self.b1_close))
-            print("min(self.a2_open, self.a2_close)", min(self.a2_open, self.a2_close))
-            if (self.b2_close > min(self.a3_open, self.a3_close) and
-                min(self.b1_open, self.b1_close) >= min(self.a2_open, self.a2_close)):
-                self.cond2 = True
-                self.time_list.extend([pd.to_datetime(self.data['time'][-1]),
-                                       pd.to_datetime(self.data['time'][-2])])
+            # case 1
+            if self.data.Low[-2] < l3:
+                b2_close = self.data.Close[-1]
+                b2_open = self.data.Open[-1]
+                b1_close = self.data.Close[-2]
+                b1_open = self.data.Open[-2]
+                if (b2_close > min(o3, c3) and
+                    min(b1_open, b1_close) >= h1 and
+                    min(b2_open, b2_close) >= h1):
+                    self.buy_a2_open = o2
+                    self.buy()
+                    print("------case 1---- 4 condition met")
+                    print("b1_low:", self.data.Low[-1])
+                    print("b1_close:", b1_close)
+                    print("b1_open:", b1_open)
+                    print("b2_close:", b2_close)
+                    print("b2_open:", b2_open)
+                    print("b_time:", self.data['time'][-1])
+                    print(related)
+                    # raise IndexError
+                    return
                 # print("cond2 is True")
-        if not self.cond2:
-            self.clean()
 
 
-        # 条件 4: 三根 bar 在同一 block
-        if self.cond1 and self.cond2 and self.cond3:
-            print("this day met all 3 conditions: ", self.data['time'][-1])
-            print("self.a1_high: ", self.a1_high,
-                  "self.a2_open:", self.a2_open,
-                  "self.a2_close:", self.a2_close,
-                  "self.a3_open:", self.a3_open,
-                  "self.a3_close:", self.a3_close,
-                  "self.a3_low:", self.a3_low,
-                  "self.b1_open:", self.b1_open,
-                  "self.b1_close:", self.b1_close,
-                  "self.b2_close:", self.b2_close,
-                  "time_list:", self.time_list,
-                  )
-
-
-            if not self.in_same_block(self.time_list):
-                self.clean()
-            else:
-                self.buy()
-                self.buy_a2_open = self.a2_open
-                print("cond4 is True, buy in with price: ", self.buy_a2_open)
 
 # --- 使用 Backtest ---
 # df 是包含 time, open, high, low, close, Moving Average 1/2/3 的 DataFrame
